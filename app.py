34d6dd6a129f2e6242071d3a9a279be8a7e0a3b6
RamonJales <ramonjales123@gmail.com>
2024-05-25 21:20:42 -0300

    search methods looking for: name, summary and content fields

--- app.py
+++ app.py
@@ -14,10 +14,27 @@ def index():
 @app.post('/')
 def handle_search():
     query = request.form.get('query', '')
-    return render_template(
-        'index.html', query=query, results=[], from_=0, total=0)
-
+    results = es.search(
+        query={
+            'multi_match': {
+                'query': query,
+                'fields': ['name', 'summary', 'content'],
+            }
+        }
+    )
+    return render_template('index.html', results=results['hits']['hits'],
+                           query=query, from_=0,
+                           total=results['hits']['total']['value'])
 
 @app.get('/document/<id>')
 def get_document(id):
-    return 'Document not found'
+    document = es.retrieve_document(id)
+    title = document['_source']['name']
+    paragraphs = document['_source']['content'].split('\n')
+    return render_template('document.html', title=title, paragraphs=paragraphs)
+
+@app.cli.command()
+def reindex():
+    response = es.reindex()
+    print(f'Index with {len(response["items"])} documents created '
+          f'in {response["took"]} milliseconds.')
